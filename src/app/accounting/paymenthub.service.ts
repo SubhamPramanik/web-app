028ba19afded774505b08028b47bb21f575164dc
SubhamPramanik <subhampramanik@gmail.com>
2020-07-06 05:59:38 +0530

    heroku dep stuff

--- src/app/accounting/paymenthub.service.ts
+++ src/app/accounting/paymenthub.service.ts
@@ -1,23 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpBackend } from '@angular/common/http';
+import { HttpClient, HttpBackend, HttpHeaders } from '@angular/common/http';
 
 /** rxjs Imports */
 import { Observable } from 'rxjs';
+
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymenthubService {
 
-  channelConnectorURL = "https://webhook.site/68282e61-9171-4fa3-a74a-471648099dfd";
+  channelConnectorURL = "https://thingproxy.freeboard.io/fetch/http://89c181596864.ngrok.io/channel/gsma/transfer";
   private http: HttpClient;
+  private headers: HttpHeaders;
 
   constructor(private handler: HttpBackend) { 
     this.http = new HttpClient(handler);
+    this.headers = new HttpHeaders({ 'Platform-TenantId': environment.fineractPlatformTenantId });
+    // this.headers.set("Fineract-Platform-TenantId", environment.fineractPlatformTenantId);
   }
 
   createGsmaTransaction(transactionRequest: any): Observable<any> {
-    return this.http.post(this.channelConnectorURL, transactionRequest, {observe: 'response'});
+    return this.http.post(this.channelConnectorURL, transactionRequest, {observe: 'response',  headers: this.headers });
   }
 
 }
